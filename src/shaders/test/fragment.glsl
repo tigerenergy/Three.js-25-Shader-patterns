f41bcf8100a6d121924c52431aaba6960a529489
kiwoon Kim <87232579+tigerenergy@users.noreply.github.com>
2021-09-08 16:03:41 +0900

    Add files via upload

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -64,9 +64,11 @@ void main()
     // float strength = barX + barY;
     // gl_FragColor = vec4(vec3(strength),1.0);
 
-    // pattern 16
-    float barX = step(0.4, mod(vUv.x * 10.0 - 0.2, 1.0)) * step(0.8, mod(vUv.y * 10.0, 1.0));
-    float barY = step(0.8, mod(vUv.x * 10.0, 1.0)) * step(0.4, mod(vUv.y * 10.0 - 0.2, 1.0));
-    float strength = barX + barY;
+    // // pattern 16
+    // float strength = abs(vUv.x - 0.5);
+    // gl_FragColor = vec4(vec3(strength),1.0);
+    
+    // pattern 17
+    float strength = min(abs(vUv.x - 0.5), abs(vUv.y - 0.5));
     gl_FragColor = vec4(vec3(strength),1.0);
     }
